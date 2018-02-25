5ae7fab174c04f6001cabea590542c2027d5a446
kazu0419 <sumalove.1214@gmail.com>
2018-02-25 16:18:10 +0900

    初めてのコミット

---
+++ Assets/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+
+	void SayHello(){
+		Debug.Log("Hello");
+	}
+
+	void Start () {
+		SayHello();
+	}
+	
+}
